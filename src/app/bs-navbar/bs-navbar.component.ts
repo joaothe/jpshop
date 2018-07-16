173dd6c3e13c11c0851b7024f489027b24e0ab40
Joao <joaoded@hotmail.com>
2018-07-16 16:53:41 -0300

    Extracting services

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,5 @@
-import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -10,14 +8,11 @@ import * as firebase from 'firebase';
 })
 export class BsNavbarComponent {
 
-  user$: Observable<firebase.User>;
-
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
+  constructor(public auth: AuthService) {
    }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 
 
