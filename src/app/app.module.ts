7a6fe7cf57e03a24b12f95c05a2ce5876576795e
Joao <joaoded@hotmail.com>
2018-07-20 13:42:12 -0300

    Starting to protect routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { AuthService } from './auth.service';
+import { AuthGuardService } from './auth-guard.service';
 
 
 @NgModule({
@@ -45,7 +46,7 @@ import { AuthService } from './auth.service';
       { path: '', component: HomeComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'check-out', component: CheckOutComponent },
+      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuardService] },
       { path: 'order-success', component: OrderSuccessComponent },
       { path: 'my/orders', component: MyOrdersComponent},
       { path: 'login', component: LoginComponent },
@@ -54,7 +55,8 @@ import { AuthService } from './auth.service';
     ])
   ],
   providers: [
-    AuthService
+    AuthService,
+    AuthGuardService
   ],
   bootstrap: [AppComponent]
 })
